47ab9287b5c83c31458c8201e8ae5bb585d9d917
alessiohajdini-sourcesense <alessio.hajdini@sourcesense.com>
2026-05-11 09:49:07 +0200

    aggiunta readme e miglioria script con aggiunta di barra di caricamento

--- ping-pong/istruzioni2.sh
+++ ping-pong/istruzioni2.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+barra_caricamento (){
+  
+  for (( i = 0; i < 60; i++ )); do
+    echo -n "█"
+    sleep 1
+  done
+  clear
+}
+
 immagine="ealen/echo-server"
 nome_c="echo"
 
 macchina="m1"
 
+clear
 echo "scarico immagine su entrambe le macchine"
-vagrant ssh m1 -c "docker pull "$immagine
-vagrant ssh m2 -c "docker pull "$immagine
+vagrant ssh m1 -c "docker pull "$immagine > /dev/null 2>&1
+vagrant ssh m2 -c "docker pull "$immagine > /dev/null 2>&1
+clear
 
 echo "avvio l'immagine sulla macchina "$macchina
-vagrant ssh $macchina -c "docker run -d --name $nome_c -p 80:80 $immagine"
+vagrant ssh $macchina -c "docker run -d --name $nome_c $immagine" > /dev/null 2>&1
 
 while true; do 
-    sleep 60
+  barra_caricamento
   
     if [ "$macchina" == m1 ]; then
         sorgente="m1"
@@ -22,18 +33,18 @@ while true; do
         sorgente="m2"
         destinazione="m1"
     fi
- 
+
     echo "migrazione da $sorgente a $destinazione"  
-    vagrant ssh $sorgente -c "
-        docker stop $nome_c &&
-        docker rm $nome_c"
+    vagrant ssh $sorgente -c "docker stop $nome_c &&  docker rm $nome_c" > /dev/null 2>&1
+    if [ $? -ne 0  ]; then
+        break
+    fi
+    clear    
         
     echo "avvio l'immagine su $destinazione"  
-    vagrant ssh $destinazione -c "docker run -d --name $nome_c -p 80:80 $immagine"
-
+    vagrant ssh $destinazione -c "docker run -d --name $nome_c $immagine" > /dev/null 2>&1
+    clear
     echo "immagine $nome_c in esecuzione su $destinazione"  
     macchina=$destinazione
-
-done
     
-
+done
